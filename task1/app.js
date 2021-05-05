8e12b335414fd524fe06f11f480df7fec988192e
Panfilenka Natallia <choco-cat@mail.ru>
2021-05-05 12:38:59 +0300

    feat: stdin, stdout

--- task1/app.js
+++ task1/app.js
@@ -1,29 +1,41 @@
 const { Command } = require('commander');
+const fs = require("fs");
+const encodeText = (text) => {
+    return `encode: ${text}`;
+}
+let encodedText = "";
 const program = new Command();
 program.version('0.0.1');
-
 program
     .option('-s, --shift <type>', 'a shift for code')
     .option('-i, --input <type>', 'an input file')
     .option('-o, --output <type>', 'an output file')
     .option('-a, --action <type>', 'an action encode/decode');
-
 program.parse(process.argv);
 
 const options = program.opts();
 
-if (Object.keys(options).length !== 0) {
-    console.log(options);
+const outputText = (encodedText) => {
+    if (options.hasOwnProperty("output")) {
+        const writeableStream = fs.createWriteStream(options.output);
+        writeableStream.write(encodedText);
+        writeableStream.end("");
+    } else {
+        process.stdout.write("DATA " + encodedText);
+    }
 }
 
-const fs = require("fs");
-
-let writeableStream = fs.createWriteStream("text.txt");
-writeableStream.write("Start!");
-writeableStream.write("Continue \n");
-writeableStream.end("End");
-let readableStream = fs.createReadStream("text.txt", "utf8");
-
-readableStream.on("data", function(chunk){
-    console.log(chunk);
-});
+if (options.hasOwnProperty("input")) {
+    const readableStream = fs.createReadStream(options.input, "utf8");
+    readableStream.on("data", function(inputText){
+        encodedText = encodeText(inputText);
+        outputText(encodedText);
+    });
+} else {
+    process.stdin.setEncoding("utf8");
+    process.stdin.on("readable", () => {
+        const inputText = process.stdin.read();
+            encodedText = encodeText(inputText);
+            outputText(encodedText);
+    });
+}
