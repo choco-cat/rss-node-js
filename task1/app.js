187e0fead6b3b423b4b8495b8ea3146d541aa43a
Panfilenka Natallia <choco-cat@mail.ru>
2021-05-05 10:05:30 +0300

    feat: parse arguments

--- task1/app.js
+++ task1/app.js
@@ -2,6 +2,20 @@ const { Command } = require('commander');
 const program = new Command();
 program.version('0.0.1');
 
+program
+    .option('-s, --shift <type>', 'a shift for code')
+    .option('-i, --input <type>', 'an input file')
+    .option('-o, --output <type>', 'an output file')
+    .option('-a, --action <type>', 'an action encode/decode');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (Object.keys(options).length !== 0) {
+    console.log(options);
+}
+
 const fs = require("fs");
 
 let writeableStream = fs.createWriteStream("text.txt");
