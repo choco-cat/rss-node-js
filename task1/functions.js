4e08a81df01bc8f1ea27549ad2cbce047af0c918
Panfilenka Natallia <choco-cat@mail.ru>
2021-05-06 09:22:17 +0300

    feat: encode different case

--- task1/functions.js
+++ task1/functions.js
@@ -4,9 +4,14 @@ const helper = {
         const letterArr = letters.split("");
         const textArr = text.trim().split("");
         const encodeTextArr = textArr.map((letter) => {
-            let index = (letterArr.indexOf(letter) + shift) % letterArr.length;
-            index = (index < 0) ? index + letterArr.length : index;
-            return letterArr[index];
+            let encodeLetter = letter;
+            const isUppercase = (letter === letter.toUpperCase()) ? true : false;
+            if (letterArr.indexOf(letter.toLocaleLowerCase()) !== -1) {
+                let index = (letterArr.indexOf(letter.toLocaleLowerCase()) + shift) % letterArr.length;
+                index = (index < 0) ? index + letterArr.length : index;
+                encodeLetter = isUppercase ? letterArr[index].toLocaleUpperCase() : letterArr[index];
+            }
+            return encodeLetter;
         });
         return encodeTextArr.join('');
     }
